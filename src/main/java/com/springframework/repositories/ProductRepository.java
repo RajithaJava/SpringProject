d0d6d0323b04ef17b37f065f23c7150c8ed70c8e
molug008 <Sridhar_Molugu@comcast.com>
2018-04-24 07:57:34 -0400

    First commit

---
+++ src/main/java/com/springframework/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.springframework.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.springframework.domain.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
